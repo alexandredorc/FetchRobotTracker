adcc88177a94126e915cbe2be1722d6fccc1db57
alexandredorc <ddorcival@gmail.com>
2022-10-18 11:48:00 +0200

    add commentaries and change tracking model

--- scripts/HSV_detec.py
+++ scripts/HSV_detec.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-from ast import Num
 import sys, math, rospy
 from geometry_msgs.msg import Twist, PoseStamped
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
-from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, Twist
 from sensor_msgs.msg import Image, PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 import tf
@@ -13,24 +12,19 @@ import ros_numpy
 
 def goal_set(goal):
     global tracker_pose
-    rospy.logwarn(goal)#   d/2cos(a)
-    nav_goal = PoseStamped()
-    nav_goal.header.frame_id = 'map'
-    rospy.loginfo(str(math.cos(tracker_pose.orientation.z)*goal[2]))
-    rospy.loginfo(str(math.sin(tracker_pose.orientation.z)*goal[0]))
-    nav_goal.pose.position.x = tracker_pose.position.x+math.cos(tracker_pose.orientation.z)*goal[2]+math.sin(tracker_pose.orientation.z)*goal[0]
-    nav_goal.pose.position.y = -tracker_pose.position.y+math.sin(tracker_pose.orientation.z)*goal[2]-math.cos(tracker_pose.orientation.z)*goal[0]
-    nav_goal.pose.orientation.z = 0
-    nav_goal.pose.orientation.w = 1
-    nav_pub.publish(nav_goal)
-    goal=[0]
-    rospy.loginfo("going to pose "+str(nav_goal.pose.position))
-
-def realCoor( x,y,dist ):
-    angle=43.5*(x-640)/640
-    angle=angle*math.pi/180
-    return [math.cos(angle) * dist, math.sin( angle ) * dist-35]
-
+    rospy.logwarn(goal)
+    d=math.sqrt(goal[2]**2+goal[0]**2) #   d/2cos(a)
+    alpha=math.atan(goal[0]/goal[2])
+    R=d/(2*math.sin(abs(alpha)))
+    v=0.5
+    w=np.sign(alpha)*v/R
+    global msg_twist
+    global time_limit_cmd
+    time_limit_cmd= (R*abs(alpha)-0.1)/(v*0.8)
+    msg_twist.linear.x=v
+    msg_twist.angular.z=-w
+    cmd_pub.publish(msg_twist)
+    rospy.loginfo(str(v)+" "+str(w)+" "+str(R)+" "+str(alpha))
 
 def display_info(rec,x,y):
     cv2.rectangle(frame, (int(rec[0]), int(rec[1])), (int(rec[0])+int(rec[2]), int(rec[3])+int(rec[1])), color_info, 2)
@@ -38,7 +32,6 @@ def display_info(rec,x,y):
     cv2.line(frame, (int(x), int(y)), (int(x)+150, int(y)), color_info, 2)
     cv2.putText(frame, "Object !!!", (int(x)+10, int(y) -10), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
             
-
 def display_images():
     stencil=cv2.bitwise_and(image, frame, mask= mask)
     cv2.putText(frame, "Couleur: {0} {1} {2}".format(color[0],color[1],color[2]), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
@@ -47,21 +40,42 @@ def display_images():
     cv2.imshow('Mask', mask)
     cv2.waitKey(3)
 
+def command_robot():
+    global time_cmd
+    global time_limit_cmd
+
+    rospy.logwarn(time_limit_cmd)
+    if time_limit_cmd>0:
+        time_cmd=time_limit_cmd+rospy.get_rostime().to_sec()
+        time_limit_cmd=0
+
+
+    
+    rospy.logerr(time_cmd)
+    rospy.logerr(rospy.get_rostime().to_sec())
+    if time_cmd>rospy.get_rostime().to_sec():
+        global msg_twist
+       
+        
+        cmd_pub.publish(msg_twist)
+    else:
+        stop_robot_cmd=Twist()
+        cmd_pub.publish(stop_robot_cmd)
+
 def image_proc(data):
     
     global frame,image,mask
     global depth_data
     global timeStamp
-    global num
     timeStamp= data.header.stamp
 
     frame = bridge.imgmsg_to_cv2(data, "bgr8")
+    HSV_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    
     image=cv2.blur(frame, (7, 7))
-    mask=cv2.inRange(image, lo_or, hi_or)
-    mask=cv2.erode(mask, None, iterations=2)
-    mask=cv2.dilate(mask, None, iterations=2)
+    mask=cv2.inRange(HSV_frame, lo_or, hi_or)
+    mask=cv2.erode(mask, None, iterations=1)
+    mask=cv2.dilate(mask, None, iterations=1)
     elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     close_elem=[]
     
@@ -70,10 +84,8 @@ def image_proc(data):
         x=int(rec[0]+(rec[2])/2)
         y=int(rec[1]+(rec[3])/2)
         
-        
         robot_shape=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
         close_elem.append(robot_shape)
-    
     
     if len(close_elem) > 0 and depth_data is not None:
         
@@ -81,14 +93,12 @@ def image_proc(data):
         y=int(rec[1]+(rec[3])/2)
 
         display_info(rec,x,y)
-        if 10<=num:
-            num=0
-            coord_robot=depth_data[y/4,x/4]
+    
+        coord_robot=depth_data[y/4,x/4]
 
-            goal_set(coord_robot)
-            #rospy.logerr(coord_robot)
-            
-    num+=1    
+        goal_set(coord_robot)
+    command_robot()
+  
     display_images()
 
     rate.sleep()
@@ -105,11 +115,17 @@ def get_tracker_pose(data):
    
     
 if __name__=="__main__":
+    global msg_twist
+    global time
+    global time_limit_cmd
+    global time_cmd
 
+    time_limit_cmd=0
+    msg_twist=Twist()
     
-
     rospy.init_node('image_proc', anonymous=True)
-    
+    time_cmd=0
+
     bridge = CvBridge()
     tfListener= tf.TransformListener()
 
@@ -118,15 +134,13 @@ if __name__=="__main__":
     color=[35,230,230]
     color_info=(0, 125, 255)
     depth_data=None
-    lo_or=np.array([0,0,150])
-    hi_or=np.array([50, 150,255])
-    global num
-    num=100
+    lo_or=np.array([10,150,150])
+    hi_or=np.array([30, 255,255])
 
     rospy.Subscriber("/head_camera/rgb/image_raw", Image, image_proc)
     rospy.Subscriber("/head_camera/depth_downsample/points", PointCloud2 , get_depth)
     rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped , get_tracker_pose)
-    nav_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped)
+    cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     
     rate=rospy.Rate(30) # spin() enter the program in a infinite loop
     rospy.spin() 
