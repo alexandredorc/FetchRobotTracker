72b8a4d3bab38778d6f698de322ae6bbd5eac46a
alexandredorc <ddorcival@gmail.com>
2022-10-18 19:12:18 +0200

    final modifications

--- scripts/HSV_detec.py
+++ scripts/HSV_detec.py
@@ -10,137 +10,145 @@ import sensor_msgs.point_cloud2 as pc2
 import tf
 import ros_numpy
 
-def goal_set(goal):
-    global tracker_pose
-    rospy.logwarn(goal)
-    d=math.sqrt(goal[2]**2+goal[0]**2) #   d/2cos(a)
-    alpha=math.atan(goal[0]/goal[2])
-    R=d/(2*math.sin(abs(alpha)))
-    v=0.5
-    w=np.sign(alpha)*v/R
-    global msg_twist
-    global time_limit_cmd
-    time_limit_cmd= (R*abs(alpha)-0.1)/(v*0.8)
-    msg_twist.linear.x=v
-    msg_twist.angular.z=-w
-    cmd_pub.publish(msg_twist)
-    rospy.loginfo(str(v)+" "+str(w)+" "+str(R)+" "+str(alpha))
+class Tracking:
+    def __init__(self):
+        #init variables for command tracker
+        self.msg_twist=Twist()
+        self.time_limit_cmd=0
+        self.time_cmd=0
 
-def display_info(rec,x,y):
-    cv2.rectangle(frame, (int(rec[0]), int(rec[1])), (int(rec[0])+int(rec[2]), int(rec[3])+int(rec[1])), color_info, 2)
-    cv2.circle(frame, (int(x), int(y)), 5, color_info, 10)
-    cv2.line(frame, (int(x), int(y)), (int(x)+150, int(y)), color_info, 2)
-    cv2.putText(frame, "Object !!!", (int(x)+10, int(y) -10), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
+        #point cloud init to None
+        self.point_cloud=None
+
+        # HSV space for image filter
+        self.lo_or=np.array([10,200,80])
+        self.hi_or=np.array([20, 255,255])
+        self.color_info=np.array([0, 165,255])
+
+        # get the raw image from camera
+        rospy.Subscriber("/head_camera/rgb/image_raw", Image, self.image_proc)
+        # get the point cloud from the stereo camera
+        rospy.Subscriber("/head_camera/depth_downsample/points", PointCloud2 , self.get_cloud)
+        # init publisher for navigation command of tracker
+        self.cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    
+    #function for command creation from pointcloud data
+    def goal_set(self ,goal_x ,goal_y):
+        #rospy.logwarn(str(goal_x)+" "+str(goal_y))
+
+        # Calculation distance between tracker and guider
+        d=math.sqrt(goal_x**2+goal_y**2)
+        # Get angle between the robot angle and tracker to guider vector 
+        alpha=math.atan(goal_x/goal_y)
+        # Calculate radius of the circle trajectory to goal
+        R=d/(2*math.sin(abs(alpha)))
+        # Fixed linear speed equivalent to turtlebot maximum speed
+        v=0.38
+        # Calculate the angular speed
+        w=np.sign(alpha)*v/R
+
+        # Calculate time from the trajectory length
+        length=R*abs(alpha)
+        # Critical distance between fetch and guider robot
+        cri_dist=0.6
+        # Calculate time of travel with acceleration of fetch robot and
+        self.time_limit_cmd= ((length-cri_dist-v**2)/v)+v
+
+        # Define command twist for tracker 
+        self.msg_twist.linear.x=v
+        self.msg_twist.angular.z=-w
+        #rospy.loginfo(str(length)+" "+str(w)+" "+str(R)+" "+str(alpha))
+
+    # Display info on camera output
+    def display_info(self,rec,x,y):
+        cv2.rectangle(self.frame, (int(rec[0]), int(rec[1])), (int(rec[0])+int(rec[2]), int(rec[3])+int(rec[1])), self.color_info, 2)
+        cv2.circle(self.frame, (int(x), int(y)), 5, self.color_info, 10)
+        cv2.line(self.frame, (int(x), int(y)), (int(x)+150, int(y)), self.color_info, 2)
+        cv2.putText(self.frame, "Guider", (int(x)+10, int(y) -10), cv2.FONT_HERSHEY_DUPLEX, 1, self.color_info, 1, cv2.LINE_AA)
+    
+    # Display camera feed mask and stencil
+    def display_images(self):
+        stencil=cv2.bitwise_and(self.image, self.frame, mask= self.mask)
+        cv2.imshow('Camera', self.frame)
+        cv2.imshow('Mask', self.mask)
+        cv2.imshow('Stencil', stencil)
+        cv2.waitKey(3)
+
+    #function for commanding the robot
+    def cmd_robot(self):
+        #rospy.logwarn(self.time_limit_cmd)
+
+        # If this variable is positive then we set a time stamp at which the robot will stop to move
+        if self.time_limit_cmd>0:
+            self.time_cmd=self.time_limit_cmd+rospy.get_rostime().to_sec()
+            self.time_limit_cmd=0
+
+        #rospy.logerr(self.time_cmd)
+        #rospy.logerr(rospy.get_rostime().to_sec())
+
+        # if the current time is lower than the time stamp set earlier we publish the command
+        if self.time_cmd>rospy.get_rostime().to_sec():
+
+            self.cmd_pub.publish(self.msg_twist)
+        else: # else we tell the robot to stop
+            stop_robot_cmd=Twist()
+            self.cmd_pub.publish(stop_robot_cmd)
+
+    # function for image processing HSV filter
+    def image_proc(self,data):
+        # Put the RGB image to BGR and create a HSV image
+        self.frame = CvBridge().imgmsg_to_cv2(data, "bgr8")
+        HSV_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+
+        # preprocessing of the image
+        self.image=cv2.blur(self.frame, (7, 7))
+        self.mask=cv2.inRange(HSV_frame, self.lo_or, self.hi_or)
+        self.mask=cv2.erode(self.mask, None, iterations=2)
+        self.mask=cv2.dilate(self.mask, None, iterations=2)
+        elements=cv2.findContours(self.mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+        size=999999
+        final_ele=None
+        for e in elements:
+            rec=cv2.boundingRect(e)
+            temp=rec[2]*rec[3]
+            if size>temp:
+                size=temp
+                final_ele=e
             
-def display_images():
-    stencil=cv2.bitwise_and(image, frame, mask= mask)
-    cv2.putText(frame, "Couleur: {0} {1} {2}".format(color[0],color[1],color[2]), (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, color_info, 1, cv2.LINE_AA)
-    cv2.imshow('Camera', frame)
-    cv2.imshow('Stencil', stencil)
-    cv2.imshow('Mask', mask)
-    cv2.waitKey(3)
-
-def command_robot():
-    global time_cmd
-    global time_limit_cmd
-
-    rospy.logwarn(time_limit_cmd)
-    if time_limit_cmd>0:
-        time_cmd=time_limit_cmd+rospy.get_rostime().to_sec()
-        time_limit_cmd=0
-
-
-    
-    rospy.logerr(time_cmd)
-    rospy.logerr(rospy.get_rostime().to_sec())
-    if time_cmd>rospy.get_rostime().to_sec():
-        global msg_twist
-       
+        if final_ele is not None and self.point_cloud is not None:
+            rec=cv2.boundingRect(final_ele)
+            x=int(rec[0]+(rec[2])/2)
+            y=int(rec[1]+(rec[3])/2)
         
-        cmd_pub.publish(msg_twist)
-    else:
-        stop_robot_cmd=Twist()
-        cmd_pub.publish(stop_robot_cmd)
+            coord_robot=self.point_cloud[y/4,x/4]
+            if coord_robot[0]==float("nan"):
+                for i in range(16):
+                    if self.point_cloud[y/4+i%4,x/4+i//4][0]!=float("nan"):
+                        coord_robot=self.point_cloud[y/4+i%4,x/4+i//4]
+                        break
+            self.goal_set(coord_robot[0],coord_robot[2])
+            self.cmd_robot()
+        else:
+            self.msg_twist.linear.x=0
+            self.msg_twist.angular.z=np.sign(self.msg_twist.angular.z)*0.7
+            self.cmd_pub.publish(self.msg_twist)
 
-def image_proc(data):
-    
-    global frame,image,mask
-    global depth_data
-    global timeStamp
-    timeStamp= data.header.stamp
-
-    frame = bridge.imgmsg_to_cv2(data, "bgr8")
-    HSV_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    image=cv2.blur(frame, (7, 7))
-    mask=cv2.inRange(HSV_frame, lo_or, hi_or)
-    mask=cv2.erode(mask, None, iterations=1)
-    mask=cv2.dilate(mask, None, iterations=1)
-    elements=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    close_elem=[]
-    
-    for e in elements:
-        rec=cv2.boundingRect(e)
-        x=int(rec[0]+(rec[2])/2)
-        y=int(rec[1]+(rec[3])/2)
+            
         
-        robot_shape=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        close_elem.append(robot_shape)
     
-    if len(close_elem) > 0 and depth_data is not None:
-        
-        x=int(rec[0]+(rec[2])/2)
-        y=int(rec[1]+(rec[3])/2)
+        self.display_images()
 
-        display_info(rec,x,y)
-    
-        coord_robot=depth_data[y/4,x/4]
-
-        goal_set(coord_robot)
-    command_robot()
-  
-    display_images()
-
-    rate.sleep()
+        self.rate.sleep()
 
      
-def get_depth(data):
-    global depth_data
-    xyz_array = ros_numpy.point_cloud2.pointcloud2_to_array(data)
-    depth_data = xyz_array
-
-def get_tracker_pose(data):
-    global tracker_pose
-    tracker_pose=data.pose.pose
-   
+    def get_cloud(self,data):
+        self.point_cloud = ros_numpy.point_cloud2.pointcloud2_to_array(data)
     
-if __name__=="__main__":
-    global msg_twist
-    global time
-    global time_limit_cmd
-    global time_cmd
-
-    time_limit_cmd=0
-    msg_twist=Twist()
-    
+if __name__=="__main__":    
     rospy.init_node('image_proc', anonymous=True)
-    time_cmd=0
-
-    bridge = CvBridge()
-    tfListener= tf.TransformListener()
-
-    robots=[]
-    confirm_robot=[]
-    color=[35,230,230]
-    color_info=(0, 125, 255)
-    depth_data=None
-    lo_or=np.array([10,150,150])
-    hi_or=np.array([30, 255,255])
-
-    rospy.Subscriber("/head_camera/rgb/image_raw", Image, image_proc)
-    rospy.Subscriber("/head_camera/depth_downsample/points", PointCloud2 , get_depth)
-    rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped , get_tracker_pose)
-    cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     
-    rate=rospy.Rate(30) # spin() enter the program in a infinite loop
+    tracker=Tracking()
+
+    tracker.rate=rospy.Rate(20) # spin() enter the program in a infinite loop
     rospy.spin() 
